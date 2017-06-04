96ee48c2db97fc6b103b45aaa77837481f2ef37e
ftmkk <ftm.karimkhani@gmail.com>
2017-06-04 14:02:48 +0430

    committed.

--- blog/models/Graph.py
+++ blog/models/Graph.py
@@ -4,15 +4,15 @@ class Graph(db.Model):
     __tablename__ = 'graph'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Unicode(200))
+    moreInfo = db.Column(db.Unicode(1000))
     isDirect = db.Column(db.Boolean)
-    isActive = db.Column(db.Boolean, default=True)
-    language = db.Column(db.Enum('فارسی', 'انگلیسی', 'عربی', 'همه‌ی زبان‌ها'))
     source = db.Column(db.Enum('tags', 'responses', 'tagsAndResponses'))
-    minWeight = db.Column(db.Float, default=0)
-    minFrequency = db.Column(db.Float, default=0)
-    startTime = db.Column(db.DateTime)
-    finishTime = db.Column(db.DateTime)
+    minEdgeWeight = db.Column(db.Float, default=0)
+    minUserOnNode = db.Column(db.Float, default=0)
+    minUserOnEdge = db.Column(db.Float, default=0)
     creationTime = db.Column(db.DateTime())
+    isActive = db.Column(db.Boolean, default=True)
+
 
 
     def getGraphList_bySource(self, source):
